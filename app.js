f0f14904a379684784ea643398d660865693930d
mishel254 <sonimichie@gmail.com>
2020-08-30 00:37:30 +0300

    description toggle

---
+++ app.js
@@ -0,0 +1,39 @@
+function thanksNote(){
+    $('.button').click( (e)=>{
+        $('.jk').show()
+
+    })
+
+}
+
+thanksNote()
+
+ $('#design').click( ()=>{
+     
+      $('#designImage').toggleClass('toggle')
+      $('#designDescription').toggleClass('content')
+ })
+
+ $('#development').click( ()=>{
+     
+    $('#developmentImage').toggleClass('toggle')
+    $('#developmentDescription').toggleClass('content')
+})
+
+$('#management').click( ()=>{
+     
+    $('#managementImage').toggleClass('toggle')
+    $('#managementDescription').toggleClass('content')
+})
+
+$('.work4').hover(
+  
+    () =>{
+        $('.hover').show()
+    } ,
+   
+
+   
+)
+
+
